66945cba579d731e97ab2f5f78127353a755b854
Chase Coltman <chase.coltman@gmail.com>
2017-10-28 16:04:00 -0700

    Updating sstf_iosched.c
    Filled in a lot of the functions that were going to be directly from the noop-iosched.c. The only two functions left will be look_dispatch and look_add_request

--- Assignment2/Kernel/sstf-iosched.c
+++ Assignment2/Kernel/sstf-iosched.c
@@ -13,41 +13,73 @@ struct sstf_data{
 	int direction;
 }
 
+//done
 static void look_merged_requests (struct request_queue *q, struct request *rq, struct request *next)
 {
     list_del_init(&next->queuelist);
 }
 
+//Needs to be done
 static int look_dispatch(struct request_queue *q, int force)
 {
 	
 }
 
+//Needs to be done
 static void look_add_request(struct request_queue *q, struct request *rq)
 {
 	
 }
 
+//done
 static struct *look_former_request(struct request_queue *q, struct request *rq)
 {
-	
+	struct sstf_data *nd = -> q->elevator_data;
+	if (rq->queuelist.prev == %nd->queue)
+		reutrn NULL;
+	return list)entry(rq->queuelist.prev, struct request, queuelist);
 }
 
+//done
 static struct *look_latter_request(struct request_queue *q, struct request *rq)
 {
-	
+	struct sstf_data *nd = q->elevator->elevator_data;
+	if(rq->queuelist.next == &nd.queue)
+		return NULL;
+	return list_entry(rq->queuelist.next, struct request, queuelist);
 }
 
+//done
 static int look_init_queue(struct request_queue *q, struct elevator_type *e)
 {
+	struct look_data *nd;
+	struct elevator_queue *eq;
 	
+	eq = elevator_alloc(q, e);
+	if (!eq)
+		return -ENOMEM;
+	if(!nd)
+	{
+		kobject_put(&eq->kobj);
+		return -ENOMEM;
+	}
+	eq->elevator_data = nd;
+	INIT_LIST_HEAD(&nd->queue);
+	spin_lock_irq(q->queue_lock);
+	q->elevator = eq;
+	spin_unlock_irq(q->queue_lock);
+	return 0;
 }
 
+//done
 static void look_exit_queue(struct elevator_queue *e)
 {
-	
+	struct look_data *nd = e->elevator_data;
+    BUG_ON(!list_empty(&nd->queue));
+    kfree(nd);
 }
 
+//done
 static struct elevator_type elevator_look = {
     .ops = {
         .elevator_merge_req_fn 		= look_merged_requests,
@@ -62,11 +94,13 @@ static struct elevator_type elevator_look = {
     .elevator_owner = THIS_MODULE,
 };
 
+//done
 static int __init noop_init(void)
 {
 	return elv_register(&elevator_noop);
 }
 
+//done
 static void __exit noop_exit(void)
 {
 	elv_unregister(&elevator_noop);
